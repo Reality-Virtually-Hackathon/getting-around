f9c8f8b96334a3dbcac6289b9c0ce94615e8a157
Vedant Saran <vedantsaran@berkeley.edu>
2017-10-07 17:33:38 -0400

    spatial audio in unity

---
+++ unityapp/New Unity Project/Assets/DropMarkers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropMarkers : MonoBehaviour {
+
+	private float nextActionTime = 0.0f;
+	public float period = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > nextActionTime ) {
+			nextActionTime += period;
+
+			// execute block of code here
+		}
+	}
+}
