990169d61400767010e8a86e0372e9f194dd4139
Dhara Bhavsar <dharapb@ccs.neu.edu>
2017-10-08 10:23:54 -0400

    commit firebase

---
+++ unityapp/New Unity Project/Assets/DropMarkers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropMarkers : MonoBehaviour {
+
+	private float nextActionTime = 0.0f;
+	public float period = 1.0f;
+	public GameObject soundOrb; 
+	public GameObject phone; 
+	public bool muteMarkers = true; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > nextActionTime ) {
+			nextActionTime += period;
+			if (muteMarkers) {
+				soundOrb.audio.mute = true; 
+			}
+				
+			Instantiate(soundOrb, phone.transform.position, phone.transform.rotation);
+
+			// execute block of code here
+		}
+	}
+}
